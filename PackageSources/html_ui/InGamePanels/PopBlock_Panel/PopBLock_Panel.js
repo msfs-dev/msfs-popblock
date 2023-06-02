e407270a7c55060824f88f40f1a641304746d7ba
msfs-dev <135231739+msfs-dev@users.noreply.github.com>
2023-06-02 11:01:43 +1000

    Update PopBLock_Panel.js
    added support of the "Offline" notification

--- PackageSources/html_ui/InGamePanels/PopBlock_Panel/PopBLock_Panel.js
+++ PackageSources/html_ui/InGamePanels/PopBlock_Panel/PopBLock_Panel.js
@@ -4,18 +4,41 @@ class PopBlockPanel extends TemplateElement {
 
     this.NewNotification = (_oData) => {
       // disconnect notification
-      // title: "Warning"
-      // type: "POPUP"
-      // 2 buttons
-      // _oData.buttons[1].title
-      // "@fs-base-onboarding,TT:MENU.SWITCH_OFFLINE"
-      // _oData.params.description = 
-      // "@fs-base-onboarding,TT:MENU.POPUP.CHECK_NETWORK_DESC"
+      /** 
+       * title: "Warning"
+       * type: "POPUP"
+       * 2 buttons
+       *  _oData.buttons[1].title
+       * "@fs-base-onboarding,TT:MENU.SWITCH_OFFLINE"
+       * _oData.params.description
+       * "@fs-base-onboarding,TT:MENU.POPUP.CHECK_NETWORK_DESC"
+       */
+     
+      // offline notification
+      /**
+       * title: "Warning"
+       * type: "POPUP"
+       * 1 button
+       * _oData.params.description
+       * "TT:MENU.POPUP.BANDWIDTH_FAILURE_WORLD_OFFLINE"
+       */
 
-      if (_oData.type == "POPUP" && _oData.title == "Warning" && _oData.buttons.length == 2) {
-        if (_oData.buttons[1].title == "@fs-base-onboarding,TT:MENU.SWITCH_OFFLINE" && _oData.params.description == "@fs-base-onboarding,TT:MENU.POPUP.CHECK_NETWORK_DESC") {
-          Coherent.trigger('NOTIFICATION_BUTTON_CLICKED', 0, _oData.id);
+      let closePopup = false;
+    
+      if (_oData.type == "POPUP" && _oData.title == "Warning") {
+        let description = _oData.params.description;
+        if(!description) {
+          description = "";
         }
+        if (_oData.buttons.length == 2 && _oData.buttons[1].title.includes("TT:MENU.SWITCH_OFFLINE") && description.includes("CHECK_NETWORK_DESC")) { // Ok/Switch Offline notification
+          closePopup = true;
+        } else if(_oData.buttons.length == 1 && description.includes("TT:MENU.POPUP.BANDWIDTH_FAILURE_WORLD_OFFLINE")) { // Ok notification after connection lost
+          closePopup = true;
+        }
+      }
+
+      if(closePopup) {
+        Coherent.trigger('NOTIFICATION_BUTTON_CLICKED', 0, _oData.id);
       }
     };
     this.m_notificationsListener = RegisterNotificationsListener();
